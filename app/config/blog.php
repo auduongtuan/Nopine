4bf3fdfd5e5acb43d8f8ac9728ebf8514feed3a1
Tuan <tanuyu@gmail.com>
2013-09-10 21:01:30 +0700

    Config

---
+++ app/config/blog.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+
+	/*
+	 * Page title & Description
+	 */
+
+	'title' => "Tuan's Blog",
+	'description' => 'Cùng nhau lắng nghe những bài nhạc hay, thưởng thức những bộ phim hấp dẫn.',
+	'sep' => ' | ',
+
+	'cache' => true,
+
+	// Post per one page
+	'perpage' => 4,
+	
+
+);
